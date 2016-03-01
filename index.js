81359cf017a41b036f735710519d9ba4f698014b
Pramod kumar <pramodkumarpara95@gmail.com>
2016-03-01 22:44:19 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,199 @@
+/**
+ * Created by pramod on 2/26/16.
+ */
+
+// import all required modules
+
+var express=require('express')
+var app = express();
+var server = require('http').createServer(app).listen(9090);
+var io = require('socket.io').listen(server);
+var mongoose = require('mongoose');
+var db = mongoose.connection;
+var schema = mongoose.schema;
+var objectId = mongoose.objectId;
+var nodeMailer=require('nodemailer');
+var crypto=require('crypto');
+var smtpTransport = require('nodemailer-smtp-transport');
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://localhost:27017/lambda_arch';
+
+
+    mongoose.connect('mongodb://localhost/lambda_arch',function(err){
+
+        if(err)
+            console.error('Connection failed ,Check if Mongo instance is started?')
+        else
+            console.log('[INFO] '+'MONGODB:Connection established successfully');
+
+    })
+
+            // allows static file serving through url, public is the root directory
+
+    app.use(express.static('public'));
+
+    app.get('/track',function(request,response){
+        response.sendFile(__dirname+'/public/js/track.js')
+    })
+
+            // home page
+
+    app.get('/home',function(request,response){
+        response.sendFile(__dirname+'/public/html/home.html');
+    });
+
+
+            // registration page
+
+    app.get('/register',function(request,response){
+        response.sendFile(__dirname+'/public/html/register.html');
+    });
+
+            //login page
+
+    app.get('/login',function(request,response){
+        response.sendFile(__dirname+'/public/html/login.html');
+    })
+
+            // reporting page
+
+    app.get('/render',function(request,response){
+        response.sendFile(__dirname+'/public/html/render.html')
+    })
+            // socket connection
+
+    io.sockets.on('connection',function(socket){
+
+
+        socket.on('send_form_data',function(data){
+
+            console.log('[USER]'+' A user registered')
+
+            var userName='pramodkumarp95',
+                password='ihavegotmyownways',
+                timeStamp=new Date().getTime();
+
+            transporter = nodeMailer.createTransport(
+                smtpTransport('smtps://pramodkumarp95%40gmail.com:ihavegotmyownways@smtp.gmail.com')
+            );
+
+            // generating API_KEYS and API_SECRET
+
+            var api_key=crypto.createHash('md5').update(data.userName+data.domainName+password).digest('hex'),
+                api_secret=crypto.createHash('md5').update(data.userName+data.domainName+password+timeStamp).digest('hex');
+
+                data.api_key=api_key;
+                data.api_secret=api_secret;
+
+                mail_options={
+                    from:'pramodkumarp95@gmail.com',
+                    to:data.email,
+                    subject: 'hello world',
+                    html:'' +
+                    '<p style="color:blue">api_key = '
+                    +api_key+
+                    '</p><br><p style="color:blue" >api_token = '+api_secret+'</p>',
+                    text:'your credentials to access raw data'
+                }
+
+            // sending email
+
+            transporter.sendMail(mail_options,function(err,response){
+                    if(err)
+                        console.error('Error sending email');
+                    else {
+                        console.log('[INFO] '+'Email sent successfully');
+                        socket.emit('success','');
+                    }
+
+            })
+
+            // connect to database
+
+
+
+            // inserting all registered users into the database
+
+            db.collection('registered_users').insert(data,function(response){
+                    console.log(response);
+
+            });
+
+        })
+
+        socket.on('custom_data',function(data){
+            // inserting all page data into the database
+            console.log(data.event_name);
+            db.collection(data.event_name.toString()).insert(data,function(response){
+                console.log(response);
+
+            });
+        })
+
+        socket.on('get_data',function(data){
+            console.log(data);
+            var domain_name=data;
+            var url = 'mongodb://localhost:27017/'+data.database_name;
+
+            MongoClient.connect(url, function(err, db) {
+
+                aggregateData(db, function() {
+                    db.close();
+                });
+
+
+            });
+
+            var aggregateData = function(db, callback) {
+                db.collection('page_views').aggregate(
+                    [
+                        //{ $match:{"domain_name":"localhost"}},
+                        { $group: { "_id": "$"+data.event, "count": { $sum: 1 } } }
+                    ]
+                ).toArray(function(err, result) {
+                    console.log(result);
+                    var data_set=[]
+                    var x=result;
+                    for(i=0;i<x.length;i++) {
+                        var temp_list = []
+                        if(x[i]['_id']==='')
+                        continue;
+                        for (var key in x[i]) {
+                            temp_list.push(x[i][key])
+                        }
+                        data_set.push(temp_list)
+                        temp_list = [];
+
+                    }
+                    console.log(result)
+                    socket.emit('send_response',data_set)
+                    callback(result);
+                });
+            };
+        })
+
+
+        socket.on('get_collection',function(){
+            var item_list=[]
+            MongoClient.connect(url, function(err, db) {
+                db.collections(function(err,collections){
+                    for(i=0;i<collections.length;i++){
+
+                        item_list.push(collections[i].namespace.toString().split('.')[1]);
+                    console.log(item_list)
+                    }
+                    socket.emit('send_collection',item_list);
+                })
+                });
+
+
+        })
+
+     // db.admin().listDatabases(function(err,items){
+       //     console.log(items);
+        //})
+
+
+
+
+    })
---
+++ public/js/track.js
@@ -0,0 +1,45 @@
+/**
+ * Created by pramod on 2/26/16.
+ */
+
+(function()
+{
+    var custom_data = {
+        screen_width: window.innerWidth,
+        screen_height: window.innerHeight,
+        screen_size: window.innerWidth + ' X ' + window.innerHeight,
+        browser_version: get_browser_info().version,
+        agent: get_browser_info().name,
+        browser: navigator.appName,
+        page_url: location.href,
+        page_name:document.title,
+        domain_name:location.hostname,
+        event_name:'page_views'
+    }
+
+    var socket = io.connect('http://localhost:9090')
+    socket.emit('custom_data', custom_data);
+})();
+
+    function get_browser_info() {
+        var ua = navigator.userAgent, tem, M = ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+        if (/trident/i.test(M[1])) {
+            tem = /\brv[ :]+(\d+)/g.exec(ua) || [];
+            return {name: 'IE', version: (tem[1] || '')};
+        }
+        if (M[1] === 'Chrome') {
+            tem = ua.match(/\bOPR\/(\d+)/)
+            if (tem != null) {
+                return {name: 'Opera', version: tem[1]};
+            }
+        }
+        M = M[2] ? [M[1], M[2]] : [navigator.appName, navigator.appVersion, '-?'];
+        if ((tem = ua.match(/version\/(\d+)/i)) != null) {
+            M.splice(1, 1, tem[1]);
+        }
+        return {
+            name: M[0],
+            version: M[1]
+        };
+    }
+
